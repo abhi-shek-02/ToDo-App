ac18f84bc881cf480c0b354a17b3a229a9b09996
Abhishek shaw <76241903+abhi-shek-02@users.noreply.github.com>
2022-07-17 16:35:29 +0530

    AllProject

--- README.md
+++ README.md
@@ -68,3 +68,79 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+// import logo from './logo.svg'
+// import './App.css'
+// import { useState } from 'react'
+
+// function App() {
+//   const [list, setlist] = useState([])
+//   const [userInput, setuserInput] = useState('')
+//   const [pendings, Setpendings] = useState(0)
+
+//   //Taking Input
+//   const functin = (e) => {
+//     setuserInput(e.target.value)
+//   }
+//   const submition = () => {
+//     setlist([...list, userInput])
+//     setuserInput('')
+//     Setpendings((prevpendings) => prevpendings + 1)
+//   }
+
+//   const deletelist = (ind) => {
+//     console.log('Hello', ind) // Error
+//     const remainingArray = list.filter((item, index) => {
+//       console.log(index)
+//       return index !== ind
+//     })
+//     console.log(remainingArray)
+//     setlist([...remainingArray])
+//   }
+
+//   const d = new Date().toLocaleString()
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h2>React ToDo List</h2>
+//       </header>
+//       <h3 style={{ color: '#405cf5' }}> PENDING TASK : {pendings}</h3>
+//       <h3 style={{ color: 'black' }}> {d}</h3>
+//       <div>
+//         {/* INPUT */}
+//         <input
+//           type="text"
+//           className="input"
+//           placeholder="Enter Your Task .."
+//           value={userInput}
+//           onChange={(e) => functin(e)}
+//         />
+//         <button onClick={() => submition()} className="button-9">
+//           Submit
+//         </button>
+//         {list.map((item, index) => {
+//           return (
+//             <div className="list-containers">
+//               <div className="list-items-main">
+//                 <div className="list-index-container">{index + 1} .</div>
+//                 <div className="list-item-container">{item}</div>
+//                 <div className="list-btn-container">
+//                   <button
+//                     className="button-11"
+//                     onClick={() => deletelist(index)}
+//                   >
+//                     Done
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           )
+//         })}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
--- src/App.js
+++ src/App.js
@@ -1,79 +1,3 @@
-// import logo from './logo.svg'
-// import './App.css'
-// import { useState } from 'react'
-
-// function App() {
-//   const [list, setlist] = useState([])
-//   const [userInput, setuserInput] = useState('')
-//   const [pendings, Setpendings] = useState(0)
-
-//   //Taking Input
-//   const functin = (e) => {
-//     setuserInput(e.target.value)
-//   }
-//   const submition = () => {
-//     setlist([...list, userInput])
-//     setuserInput('')
-//     Setpendings((prevpendings) => prevpendings + 1)
-//   }
-
-//   const deletelist = (ind) => {
-//     console.log('Hello', ind) // Error
-//     const remainingArray = list.filter((item, index) => {
-//       console.log(index)
-//       return index !== ind
-//     })
-//     console.log(remainingArray)
-//     setlist([...remainingArray])
-//   }
-
-//   const d = new Date().toLocaleString()
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h2>React ToDo List</h2>
-//       </header>
-//       <h3 style={{ color: '#405cf5' }}> PENDING TASK : {pendings}</h3>
-//       <h3 style={{ color: 'black' }}> {d}</h3>
-//       <div>
-//         {/* INPUT */}
-//         <input
-//           type="text"
-//           className="input"
-//           placeholder="Enter Your Task .."
-//           value={userInput}
-//           onChange={(e) => functin(e)}
-//         />
-//         <button onClick={() => submition()} className="button-9">
-//           Submit
-//         </button>
-//         {list.map((item, index) => {
-//           return (
-//             <div className="list-containers">
-//               <div className="list-items-main">
-//                 <div className="list-index-container">{index + 1} .</div>
-//                 <div className="list-item-container">{item}</div>
-//                 <div className="list-btn-container">
-//                   <button
-//                     className="button-11"
-//                     onClick={() => deletelist(index)}
-//                   >
-//                     Done
-//                   </button>
-//                 </div>
-//               </div>
-//             </div>
-//           )
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
 import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
@@ -94,14 +18,12 @@ function App() {
   }
 
   const deletelist = (ind) => {
-    console.log('Hello', ind) // Error
     Setpendings((prevpendings) => prevpendings - 1)
-
     const remainingArray = list.filter((item, index) => {
-      console.log(index)
+      // console.log(index)
       return index !== ind
     })
-    console.log(remainingArray)
+    // console.log(remainingArray)
     setlist([...remainingArray])
   }
 
@@ -128,7 +50,7 @@ function App() {
         </button>
         {list.map((item, index) => {
           return (
-            <div className="list-containers">
+            <div className="list-containers" key={index}>
               <div className="list-items-main">
                 <div className="list-index-container">{index + 1} .</div>
                 <div className="list-item-container">{item}</div>
