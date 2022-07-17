1a9533217fada4cea7dbeeb374544e395df011d0
Abhishek shaw <76241903+abhi-shek-02@users.noreply.github.com>
2022-07-17 14:20:14 +0530

    Namak

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  const [list, setlist] = useState([])
+  const [userInput, setuserInput] = useState("")
+  const [pendings, Setpendings] = useState(0)
+
+  //Taking Input
+  const functin = (e) => {
+    setuserInput(e.target.value)
+  }
+  const submition = () => {
+    setlist([...list, userInput])
+    Setpendings(prevpendings => prevpendings + 1)
+  }
+  // -----------
+  const deletelist = (ind) => {
+    console.log("Hello", ind) // Error
+    const remainingArray = list.filter((item, index) => {
+      console.log(index)
+      return index !== ind
+    })
+    console.log(remainingArray)
+    setlist([remainingArray])
+  }
+  // -----------
+  const d = new Date().toLocaleString()
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React ToDo List</h2>
+      </header>
+      <h3 style={{ color: "#405cf5" }}> PENDING TASK : {pendings}</h3>
+      <h3 style={{ color: "black" }}> {d}</h3>
+      <div>
+        {/* INPUT */}
+        <input type="text" className="input" placeholder="Enter Your Task .." onChange={(e) => functin(e)} />
+        <button onClick={() => submition()} className="button-9">Submit</button>
+        {
+          list.map((item, index) => {
+            return (
+              <div className="list-containers">
+                <div className="list-items-main" >
+                  <div className="list-index-container">{index + 1} .</div>
+                  <div className="list-item-container">{item}</div>
+                  <div className="list-btn-container">
+                    {/* {console.log(index)} */}
+                    <button className="button-11" onClick={(index) => deletelist(index)}>Done</button>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/List.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import '../App.css';
+// import {useState} from 'react'
+// import {userContext} from 'react'
+// export const UserContext = React.createContext()
+
+// const List = ({ index, item }) => {
+//     const [ind,Setind] = useState(0);
+
+//     const done = (index) => {
+//         Setind(2)
+//     }
+    
+
+//     return (
+//       <div className="list-items-main" >
+//         <div className="list-index-container">{index + 1} .</div>
+//         <div className="list-item-container">{item}</div>
+//         <div className="list-btn-container">
+//           <button className="button-11" onClick={(index) => done(index)}>Done</button>
+//         </div>
+//       </div>
+//     )
+//   }
+
+// export default List
+
